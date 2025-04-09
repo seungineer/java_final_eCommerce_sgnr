6a3625abf5ede5ba7d8c7be34c3ac98b30a8cd12
seungineer <97353541+seungineer@users.noreply.github.com>
2025-04-09 10:21:17 +0900

    feature display product without login

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import view.LoginView;
+import view.ProductListView;
 import view.SignupView;
 
 public class Main {
@@ -7,18 +8,22 @@ public class Main {
 
         while (isRunning) {
             System.out.println("\n=== e-Commerce 시스템 ===");
-            System.out.println("1. 로그인");
-            System.out.println("2. 회원가입");
+            System.out.println("1. 비회원으로 상품 목록 보기");
+            System.out.println("2. 로그인");
+            System.out.println("3. 회원가입");
             System.out.println("0. 종료");
             System.out.print("선택: ");
 
             String input = new java.util.Scanner(System.in).nextLine();
             switch (input) {
                 case "1":
+                    ProductListView.render();
+                    break;
+                case "2":
                     LoginView loginView = new LoginView();
                     loginView.render();
                     break;
-                case "2":
+                case "3":
                     SignupView signupView = new SignupView();
                     signupView.render();
                     break;
--- src/controller/AdminProductController.java
+++ src/controller/AdminProductController.java
@@ -8,8 +8,8 @@ public class AdminProductController {
     private ProductDAO productDAO = new ProductDAO();
 
     public String diplayProductName(Product product) {
-        if (product.getSaleStatus() == 0) return "[판매 중지] " + product.getProductName();
-        if (product.getStock() == 0) return "[품절] " + product.getProductName();
+        if (product.getSaleStatus() == 0) return "<판매 중지> " + product.getProductName();
+        if (product.getStock() == 0) return "<품절> " + product.getProductName();
         return product.getProductName();
     }
 
